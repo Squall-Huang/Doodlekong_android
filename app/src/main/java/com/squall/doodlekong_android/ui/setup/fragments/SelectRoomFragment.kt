090049a4c0be569dd18ba7ed19b5fc1f60d89354
Squall Huang <selfi_star@yahoo.com.tw>
2021-07-22 18:13:45 +0800

    part37 SelectRoomFragment

--- app/src/main/java/com/squall/doodlekong_android/ui/setup/fragments/SelectRoomFragment.kt
+++ app/src/main/java/com/squall/doodlekong_android/ui/setup/fragments/SelectRoomFragment.kt
@@ -4,14 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.squall.doodlekong_android.R
+import com.squall.doodlekong_android.adapters.RoomAdapter
 import com.squall.doodlekong_android.databinding.FragmentSelectRoomBinding
-import com.squall.doodlekong_android.databinding.FragmentUsernameBinding
+import com.squall.doodlekong_android.ui.setup.SetupViewModel
+import com.squall.doodlekong_android.ui.setup.SetupViewModel.SetupEvent.*
+import com.squall.doodlekong_android.util.Constants.SEARCH_DELAY
+import com.squall.doodlekong_android.util.navigateSafely
+import com.squall.doodlekong_android.util.snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SelectRoomFragment : Fragment() {
 
     private var _binding: FragmentSelectRoomBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by activityViewModels<SetupViewModel>()
+
+    private val args by navArgs<SelectRoomFragmentArgs>()
+
+    @Inject
+    lateinit var roomAdapter: RoomAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,12 +48,103 @@ class SelectRoomFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        subscribeToObservers()
+        listenToEvents()
+
+        viewModel.getRooms("")
+
+
+        var searchJob: Job? = null
+        binding.etRoomName.addTextChangedListener {
+            searchJob?.cancel()
+            searchJob = lifecycleScope.launch {
+                delay(SEARCH_DELAY)
+                viewModel.getRooms(it.toString())
+            }
+        }
+
+        binding.apply {
+            ibReload.setOnClickListener {
+                roomsProgressBar.isVisible = true
+                ivNoRoomsFound.isVisible = false
+                tvNoRoomsFound.isVisible = false
+                viewModel.getRooms(etRoomName.text.toString())
+            }
+        }
+
+        binding.btnCreateRoom.setOnClickListener {
+            findNavController().navigateSafely(
+                R.id.action_selectRoomFragment_to_createRoomFragment,
+                Bundle().apply { putString("username",args.username) }
+            )
+        }
+        roomAdapter.setOnRoomClickListener {
+            viewModel.joinRoom(args.username, it.name)
+        }
+    }
+
+    private fun listenToEvents() = lifecycleScope.launchWhenStarted {
+        viewModel.setupEvent.collect { event ->
+            when (event) {
+                is JoinRoomEvent -> {
+                    findNavController().navigateSafely(
+                        R.id.action_selectRoomFragment_to_drawingActivity,
+                        args = Bundle().apply {
+                            putString("username", args.username)
+                            putString("roomName", event.roomName)
+                        }
+                    )
+                }
+                is JoinRoomErrorEvent -> {
+                    snackbar(event.error)
+                }
+                is GetRoomErrorEvent -> {
+                    binding.apply {
+                        roomsProgressBar.isVisible = false
+                        tvNoRoomsFound.isVisible = false
+                        ivNoRoomsFound.isVisible = false
+                    }
+                    snackbar(event.error)
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    private fun subscribeToObservers() = lifecycleScope.launchWhenStarted {
+        viewModel.rooms.collect { event ->
+            when (event) {
+                GetRoomLoadingEvent -> {
+                    binding.roomsProgressBar.isVisible = true
+                }
+                is GetRoomEvent -> {
+                    binding.roomsProgressBar.isVisible = false
+                    val isRoomsEmpty = event.rooms.isEmpty()
+                    binding.tvNoRoomsFound.isVisible = isRoomsEmpty
+                    binding.ivNoRoomsFound.isVisible = isRoomsEmpty
+                    lifecycleScope.launch {
+                        roomAdapter.updateDataset(event.rooms)
+                    }
+                }
+                else -> Unit
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvRooms.apply {
+            adapter = roomAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 }
--- app/src/main/java/com/squall/doodlekong_android/util/Constants.kt
+++ app/src/main/java/com/squall/doodlekong_android/util/Constants.kt
@@ -13,5 +13,5 @@ object Constants {
     const val MIN_ROOM_NAME_LENGTH = 4
     const val MAX_ROOM_NAME_LENGTH = 16
 
-
+    const val SEARCH_DELAY = 300L
 }
