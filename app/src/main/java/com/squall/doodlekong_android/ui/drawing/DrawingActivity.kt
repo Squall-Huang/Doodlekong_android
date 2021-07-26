87f28c3e75ab3e1095dc2f5ec1d1fcee2c0caa96
Squall Huang <selfi_star@yahoo.com.tw>
2021-07-26 10:42:53 +0800

    Part42-DrawingViewBasicSetup

--- app/src/main/java/com/squall/doodlekong_android/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/squall/doodlekong_android/ui/drawing/DrawingActivity.kt
@@ -1,12 +1,16 @@
 package com.squall.doodlekong_android.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.squall.doodlekong_android.R
 import com.squall.doodlekong_android.databinding.ActivityDrawingBinding
+import com.squall.doodlekong_android.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -30,13 +34,37 @@ class DrawingActivity : AppCompatActivity() {
             }
         }
     }
-    
+
+    private fun selectColor(color: Int): Unit {
+        binding.apply {
+            drawingView.setColor(color)
+            drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
+        }
+    }
+
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.selectedColorButtonId.collect { id ->
                     binding.apply {
                         colorGroup.check(id)
+                        when (id) {
+                            R.id.rbBlack -> selectColor(Color.BLACK)
+                            R.id.rbBlue -> selectColor(Color.BLUE)
+                            R.id.rbGreen -> selectColor(Color.GREEN)
+                            R.id.rbOrange -> selectColor(
+                                ContextCompat.getColor(
+                                    this@DrawingActivity,
+                                    R.color.orange
+                                )
+                            )
+                            R.id.rbRed -> selectColor(Color.RED)
+                            R.id.rbYellow -> selectColor(Color.YELLOW)
+                            R.id.rbEraser -> {
+                                drawingView.setColor(Color.WHITE)
+                                drawingView.setThickness(40f)
+                            }
+                        }
                     }
                 }
             }
